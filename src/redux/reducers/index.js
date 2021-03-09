dffffe84009f0820c22e15a582df0638556cae7b
Eyoel Demissie <eyoel@attestiv.com>
2021-03-08 21:26:33 -0500

    added redux store along with redux devtools extension to view/debug state

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+// import visibilityFilter from "./visibilityFilter";
+// import todos from "./todos";
+import initialState from './initialState';
+
+
+//TODO: add reducers in the code below.
+export default combineReducers({ initialState });
